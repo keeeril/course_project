aa142f5c1a835428088eecfb008d70fe558fb4bc
Kirill <keeeril@bk.ru>
2021-12-05 21:30:57 +0300

    modify

--- Project.py
+++ Project.py
@@ -19,10 +19,15 @@ params = {
 files = {}
 
 response = requests.get(url, params=params)
-response_outfit = response.json()['response']['items']
-for albums in response_outfit:
+response_outfit = response.json()
+for albums in response_outfit['response']['items']:
     # print(albums)
     ph_name = albums['likes']['count']
+    if ph_name in files:
+        ph_name = str(ph_name) + ' ' + str(albums['date'])
+    else:
+        ph_name = ph_name
+    # print(ph_name)
     for i in albums['sizes']:
         if 'w' in i['type']:
             ph_url = i['url']
@@ -33,23 +38,25 @@ for albums in response_outfit:
         elif 'r' in i['type']:
             ph_url = i['url']
     files[ph_name] = ph_url
-# print(len(files))
-# pprint(files)
 
 with open('/Users/keeeril/Desktop/Yandex_token.txt', 'rt', encoding='utf-8') as f:
     yandex = f.readline()
+counter = 0
+for name_photo, file_url in files.items():
+    headers = {
+        "Accept": "application/json",
+        "Authorization": "OAuth " + yandex
+    }
 
-headers = {
-    "Accept": "application/json",
-    "Authorization": "OAuth " + yandex
-}
+    params = {
+        'path':'course/' + str(name_photo),
+        'url': file_url
+    }
 
-params = {
-    'path':'course' + str(ph_name),
-    'url': files[ph_name]
-}
-
-url = "https://cloud-api.yandex.net/v1/disk/resources/upload/"
-r = requests.post(url=url, params=params, headers=headers)
-res = r.json()
-print(json.dumps(res, sort_keys=True, indent=4, ensure_ascii=False))
+    url = "https://cloud-api.yandex.net/v1/disk/resources/upload/"
+    r = requests.post(url=url, params=params, headers=headers)
+    res = r.json()
+    counter += 1
+    print(f'Загружено {counter} из {len(files)}')
+print('Загрузка завершена!')
+    # print(json.dumps(res, sort_keys=True, indent=4, ensure_ascii=False))
